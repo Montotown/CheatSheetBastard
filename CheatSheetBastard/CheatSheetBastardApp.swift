bb58391d51390c1fed3ad89513d177e175d07244
Per Ollas <montotown@gmail.com>
2021-07-12 13:28:27 +0200

    Initial Commit

---
+++ CheatSheetBastard/CheatSheetBastardApp.swift
@@ -0,0 +1,17 @@
+//
+//  CheatSheetBastardApp.swift
+//  CheatSheetBastard
+//
+//  Created by Per Ollas on 2021-07-12.
+//
+
+import SwiftUI
+
+@main
+struct CheatSheetBastardApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
